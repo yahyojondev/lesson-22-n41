7a8c7a16f9a66f922acdba24097eb11db0cd1dab
yahyojondev <nabijonovdeveloper@gmail.com>
2023-12-01 05:32:42 +0500

    lesson22

---
+++ js/main.js
@@ -0,0 +1,88 @@
+const loading = document.getElementById("loading");
+
+const loadingDuration = 1000; // 1s
+
+setTimeout(() => {
+  loading.classList.add("loading-none");
+}, loadingDuration); 
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.classList.add('backtop-show')
+  } else {
+    backtop.classList.remove('backtop-show')
+  }
+}
+
+function openNavbar() {
+  document.getElementById("navbar-responsive").style.left = "0";
+  // document.getElementById("navbar-responsive").style.top = "0";
+}
+function closeNavbar() {
+  document.getElementById("navbar-responsive").style.left = "-100%";
+  // document.getElementById("navbar-responsive").style.top = "-100%";
+}
+
+document.getElementById("navbar-open").addEventListener("click", openNavbar);
+document.getElementById("navbar-close").addEventListener("click", closeNavbar);
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+		navbar.classList.remove("navbar-shrink");
+	}
+}
+
+let modeBtn = document.getElementById("mode-btn");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "images/light.svg";
+  } else {
+    this.firstElementChild.src = "images/dark.svg";
+  }
+  document.body.classList.toggle("dark");
+});
+
+
+$(document).ready(function () {
+  $("#team-carousel").owlCarousel({
+    margin: 30,
+    loop: true,
+    nav: true,
+    navText: ['<img src="images/prev.svg"/>', '<img src="images/next.svg"/>'],
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 10000,
+    responsive: {
+      0: {
+        items: 1,
+        dots: false,
+      },
+      600: {
+        items: 2,
+      },
+      900: {
+        items: 3,
+      },
+      1200: {
+        items: 4,
+      },
+    },
+  });
+});
